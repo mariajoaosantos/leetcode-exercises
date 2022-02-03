4275b3c13090b7c70d33d7b8c46f481218923551
mariajoaosantos <mariajcs18@gmail.com>
2022-02-03 22:26:00 +0000

    Solution for 1 - TwoSum

--- 1-twosum.js
+++ 1-twosum.js
@@ -1,12 +1,9 @@
 var twoSum = function(nums, target) {
-    let mult;
     let output;
-    let i;
-    let j;
-    for (let i=0; i < nums.length-1; i++) {
-        for (let j=0; j<nums.length-1; j++) {
-            mult = nums[i]+nums[j]
-            if (mult === target) {
+    for (let i=0; i < nums.length; i++) {
+        for (let j=i+1; j< nums.length; j++) {
+            let sum = nums[i]+nums[j]
+            if (sum === target && j != i) {
                 output = [i,j]
             }
         }
